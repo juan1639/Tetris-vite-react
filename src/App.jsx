d3905686c3d524ba908e26a9d22c0a82f6e57cae
unknown <juaneguiabad1639@gmail.com>
2024-03-14 01:23:26 +0100

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex w-screen min-h-screen justify-center items-center bg-neutral-600'>
+        <h1 className='text-slate-200 text-2xl font-bold'>Suartxetxeifus & La hormiga</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
