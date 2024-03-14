1e346233438739c58986a4527a2791b92f4d67d0
unknown <juaneguiabad1639@gmail.com>
2024-03-15 00:30:49 +0100

    creando la matriz fondo (grid)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,60 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { GRID } from './settings'
+import { GRID, plantilla, attrPieza } from './settings'
 
 function App()
 {
-  const [count, setCount] = useState(0)
+  const [fondo, setFondo] = useState(GRID)
+
+  const piezaActual = () =>
+  {
+    for (let i = 0; i < 4; i ++)
+    {
+      const offSetX = plantilla.z[i][0]
+      const offSetY = plantilla.z[i][1]
+
+      const {x, y} = attrPieza;
+      const updateFondo = [...fondo]
+      updateFondo[y + offSetY][x + offSetX] = 5
+      setFondo(updateFondo)
+    }
+
+    /* plantilla.z[0].forEach(segm =>
+      {
+        const {x, y} = attrPieza;
+        const updateFondo = [...fondo]
+        console.log(segm, y, x)
+        updateFondo[y + segm[1]][x + segm[0]] = 5
+        setFondo(updateFondo)
+      }); */
+  }
+
+  // console.log(fondo)
 
   return (
     <>
-      <main className='flex w-screen min-h-screen justify-center items-center bg-neutral-600'>
-        <div className='grid grid-cols-14 grid-rows-19 w-[420px] h-[570px] bg-red-500'>
+      <main className='flex w-screen min-h-screen justify-center items-center bg-neutral-600' onClick={piezaActual}>
+        <div className='grid grid-cols-14 grid-rows-19 w-[392px] h-[532px] bg-red-500 border-slate-100 border-solid border'>
           {
             GRID.map((fila, index) =>
             {
               const coorY = `row-start-${index}`
 
               return (
-                fila.map((columna, index) =>
+                fila.map((columna, i) =>
                 {
-                  const coorX = `col-start-${index}`
-                  const coorXY = coorX + ' ' + coorY
+                  const coorX = `col-start-${i}]`
+                  const estilo = `${coorX} ${coorY} border border-blue-600 border-solid`
 
-                  return (
-                    <span className={coorXY}>{coorXY}</span>
-                  )
+                  return <Casilla
+                      estilo={estilo}
+                      key={i + index}
+                      fondo={fondo}
+                      setFondo={setFondo}
+                      index={index}
+                      i={i}
+                    >{fondo[index][i]}</Casilla>
                 })
               )
             })
@@ -33,6 +63,18 @@ function App()
       </main>
     </>
   )
+}
+
+const Casilla = ({estilo, children, fondo, setFondo, index, i}) =>
+{
+  const handleClick = () =>
+  {
+    const updateFondo = [...fondo]
+    updateFondo[index][i] = 0
+    setFondo(updateFondo)
+  }
+
+  return <span className={estilo} onClick={handleClick}>{children}</span>
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '14': 'repeat(14, minmax(0, 1fr))',
+      },
+      gridTemplateRows: {
+        '19': 'repeat(19, minmax(0, 1fr))',
+      }
+    },
   },
   plugins: [],
 }
